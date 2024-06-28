5af069d4196fd4ef13e59de5738a4fa21a3f448b
Logan <logan.zchjones@gmail.com>
2024-06-27 18:29:26 -0700

    added 2 twoLayer

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from model import *
+# from model import *
+from twoLayer import *
 def main():
     if __name__ == '__main__':
-        w1, b1, w2, b2 = gradient_descent(x_train, labels_train, 1500, .12)
+        rate = .5
+        iters = 1250
+        w1, b1, w2, b2, w3, b3 = gradient_descent(x_train, labels_train, iters, rate)
         
 
-        preds = make_predictions(x_test, w1, b1, w2, b2)
-        print(f'test accuracy : {get_accuracy(preds, labels_test)}')
+        preds = make_predictions(x_test, w1, b1, w2, b2, w3, b3)
+        print(f'test accuracy : {get_accuracy(preds, labels_test)} \n Learning Rate: {rate} Iterations : {iters}')
 
 main()
